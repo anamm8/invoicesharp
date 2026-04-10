f0f4981f3bac13866cde5b3c5e16b96aa1d5c5da
Matheus <matdavykk@gmail.com>
2026-04-11 00:47:05 +0100

    feat: Added models and config entity framework to support sqlite db

---
+++ src/InvoiceSharp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InvoiceSharp.Models;
+
+namespace InvoiceSharp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Suas tabelas
+        public DbSet<ClientModel> Clients { get; set; }
+        public DbSet<InvoiceModel> Invoices { get; set; }
+        public DbSet<InvoiceItemModel> InvoiceItems { get; set; }
+    }
+}
