db1ff148648066fe276c3288823a11a74578fb99
kaiky <kaiky@LAPTOP-6O049LNI>
2026-04-15 18:10:24 +0100

    Fix missing ClientsController to enable /Clients/Create route

---
+++ src/InvoiceSharp/Controllers/ClientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceSharp.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
