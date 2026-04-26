4e35c804fea6f5d72dd9142fc1145974865d1810
Ricardo Costa <134674414+Rtheghost@users.noreply.github.com>
2026-04-26 03:00:26 +0100

    Refactor ClientsController to use 'client' variable

--- src/InvoiceSharp/Controllers/ClientsController.cs
+++ src/InvoiceSharp/Controllers/ClientsController.cs
@@ -34,10 +34,10 @@ namespace InvoiceSharp.Controllers
                 .AsNoTracking()
                 .FirstOrDefaultAsync(c => c.Id == id);
 
-            if (clients == null)
+            if (client == null)
                 return NotFound();
 
-            return View(clients);
+            return View(client);
         }
 
         // GET: /Clients/Create
@@ -52,7 +52,7 @@ namespace InvoiceSharp.Controllers
         public async Task<IActionResult> Create(ClientModel client)
         {
             if (!ModelState.IsValid)
-                return View(clients);
+                return View(client);
 
             _context.Clients.Add(client);
             await _context.SaveChangesAsync();
@@ -66,12 +66,12 @@ namespace InvoiceSharp.Controllers
             if (id == null)
                 return NotFound();
 
-            var clients = await _context.Clients.FindAsync(id);
+            var client = await _context.Clients.FindAsync(id);
 
-            if (clients == null)
+            if (client == null)
                 return NotFound();
 
-            return View(clients);
+            return View(client);
         }
 
         // POST: /Clients/Edit/5
@@ -79,20 +79,20 @@ namespace InvoiceSharp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, ClientModel client)
         {
-            if (id != clients.Id)
+            if (id != client.Id)
                 return NotFound();
 
             if (!ModelState.IsValid)
-                return View(clients);
+                return View(client);
 
             try
             {
-                _context.Update(clients);
+                _context.Update(client);
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
-                bool exists = await _context.Clients.AnyAsync(c => c.Id == clients.Id);
+                bool exists = await _context.Clients.AnyAsync(c => c.Id == client.Id);
                 if (!exists)
                     return NotFound();
 
@@ -112,10 +112,10 @@ namespace InvoiceSharp.Controllers
                 .AsNoTracking()
                 .FirstOrDefaultAsync(c => c.Id == id);
 
-            if (clients == null)
+            if (client == null)
                 return NotFound();
 
-            return View(clients);
+            return View(client);
         }
 
         // POST: /Clients/Delete/5
@@ -125,9 +125,9 @@ namespace InvoiceSharp.Controllers
         {
             var client = await _context.Clients.FindAsync(id);
 
-            if (clients != null)
+            if (client != null)
             {
-                _context.Clients.Remove(clients);
+                _context.Clients.Remove(client);
                 await _context.SaveChangesAsync();
             }
 
