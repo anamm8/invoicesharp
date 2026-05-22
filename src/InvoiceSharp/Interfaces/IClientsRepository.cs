f16bae5f5822c405ea7f37ebabd8d9e574e97efe
Matheus <matdavykk@gmail.com>
2026-05-22 17:50:43 +0100

    remove comment

--- src/InvoiceSharp/Interfaces/IClientsRepository.cs
+++ src/InvoiceSharp/Interfaces/IClientsRepository.cs
@@ -5,11 +5,17 @@ namespace InvoiceSharp.Interfaces
     public interface IClientsRepository
     {
         Task<IEnumerable<ClientModel>> GetAllAsync();
+
         Task<ClientModel?> GetByIdAsync(int id);
+
         Task AddAsync(ClientModel client);
+
         void Update(ClientModel client);
+
         Task DeleteAsync(int id);
+
         Task<bool> ExistsAsync(int id);
+
         Task<bool> SaveChangesAsync();
     }
 }
